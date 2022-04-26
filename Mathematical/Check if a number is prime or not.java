e8cdb87a98b44355d8ee7bf9412ea0ef1ab40d20
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-26 21:48:50 +0530

    Create Check if a number is prime or not.java

---
+++ Mathematical/Check if a number is prime or not.java
@@ -0,0 +1,22 @@
+class Solution {
+
+  public static boolean isPrime(int N) {
+
+    for (int i = 2; i < Math.sqrt(N); i++) {
+      if (N % i == 0) {
+        return false;
+      }
+    }
+    return true;
+
+  }
+  public static void main(String args[]) {
+    int n = 20;
+    boolean ans = (isPrime(n));
+    if (ans == true) {
+      System.out.println("Prime Number");
+    } else {
+      System.out.println("Non Prime Number");
+    }
+  }
+}

df7e74a35c204d833f74abac2c06c2f0ad06002c
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-26 21:48:13 +0530

    Create Find all Palindrome Numbers in a given range.java

---
+++ Mathematical/Find all Palindrome Numbers in a given range.java
@@ -0,0 +1,30 @@
+import java.util.*;
+ 
+public class tuf {
+      static boolean isPalindrome(int n)
+        {
+             
+            int reverse = 0;
+            int temp = n;
+            while(temp>0) {
+                reverse = reverse*10 + temp%10;
+                temp = temp/10;
+            }
+                 
+            
+           if(n==reverse)return true;
+           return false;
+        }
+       
+        public static void main(String args[])
+        {
+            int min = 100;
+            int max = 150;
+            for(int i=min; i<=max; i++) {
+                if(isPalindrome(i)) {
+                    System.out.print(i + " ");
+                }
+            }
+     
+        }
+}

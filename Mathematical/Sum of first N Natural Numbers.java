ec233d338ee7ef0a04faca8823a68d9bce835c85
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-27 07:50:20 +0530

    Create Sum of first N Natural Numbers.java

---
+++ Mathematical/Sum of first N Natural Numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class tuf {
+
+  public static void main(String[] args) {
+
+    solve(5);
+    solve(6);
+  }
+  public static void solve(int N) {
+    int sum = N * (N + 1) / 2;
+    System.out.println("The sum of the first " + N + " numbers is: " + sum);
+  }
+}

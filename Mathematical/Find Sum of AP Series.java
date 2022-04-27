a1c7c10c43ed1e15c8a59e7643fc1bc3ddf03ec8
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-27 07:52:13 +0530

    Create Find Sum of AP Series.java

---
+++ Mathematical/Find Sum of AP Series.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class TUF{
+static double sumofAp(double a, double d, int n)
+{
+   double sum = (n / 2.0) * (2.0 * a + (n - 1) * d);
+    return sum;
+}
+public static void main(String args[])
+{
+    double a = 1.5, d = 3;
+    int n = 5;
+    System.out.println("Sum of Given Ap Series: "+sumofAp(a, d, n));
+}
+}

105d5faa936dd579d14c98b210f88aeb428f5b63
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-27 07:49:49 +0530

    Create Check whether a number is positive or negative.java

---
+++ Mathematical/Check whether a number is positive or negative.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class tuf {
+
+  public static void main(String[] args) {
+
+    int n = 5;
+    check(n);
+    n = -6;
+    check(n);
+  }
+  public static void check(int n) {
+   if (n >> 31 == 0) {
+      System.out.println(n + " is positive.");
+    } else if (n >> 31 == -1) {
+      System.out.println(n + " is negative.");
+    }

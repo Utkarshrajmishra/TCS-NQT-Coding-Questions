8f0df5bdc117c8e639f9ae91e3c4f22e73b01dab
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-27 19:39:51 +0530

    Create Convert Binary to Octal.java

---
+++ Number System/Convert Binary to Octal.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main(String args[]) {
+    String s = "1100110";
+    int n = s.length();
+   
+    if (n % 3 == 1) {
+      s = "00" + s;
+    } else if (n % 3 == 2) {
+      s = "0" + s;
+    }
+    n = s.length();
+    String ans = "";
+    for (int i = 0; i < n; i += 3) {
+      int temp = (s.charAt(i) - '0') * 4 + (s.charAt(i + 1) - '0') * 2 + (s.charAt(i + 2) - '0') * 1;
+      ans = ans + Integer.toString(temp);
+    }
+    System.out.print(ans);
+  }
+}

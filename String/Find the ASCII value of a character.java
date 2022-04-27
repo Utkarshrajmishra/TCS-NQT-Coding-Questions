4de65e5da7b2687da283aa3368b67b88dcc666ed
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-27 19:51:47 +0530

    Create Find the ASCII value of a character.java

---
+++ String/Find the ASCII value of a character.java
@@ -0,0 +1,13 @@
+class Solution {
+
+  public static void value() {
+
+    char c = 'e';
+    int ascii = c;
+    System.out.println("The ASCII value of " + c + " is: " + ascii);
+  }
+  public static void main(String args[]) {
+
+    value();
+  }
+}

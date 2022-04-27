ca8b5291ac5a6ecd056000588d39f434e652a866
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-27 19:34:57 +0530

    Create Calculate the Area of the Circle.java

---
+++ Mathematical/Calculate the Area of the Circle.java
@@ -0,0 +1,13 @@
+class Solution {
+
+  public static void areaOfCircle(int n) {
+
+    double ans = 3.14 * n * n; // Area of circle = πr2
+    System.out.print("Area of circle is : " + ans);
+
+  }
+  public static void main(String args[]) {
+    int n = 5;
+    areaOfCircle(n);
+  }
+}

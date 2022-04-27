4b89d6b85652675c449f9cfaa6b0df08c78f7b6a
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-27 19:34:21 +0530

    Create Express given number as Sum of Two Prime Numbers.java

---
+++ Mathematical/Express given number as Sum of Two Prime Numbers.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Solution {
+
+       static boolean prime(int n) {
+
+              if (n <= 1)
+                     return false;
+
+              for (int i = 2; i * i <= n; i++) {
+                     if (n % i == 0) {
+                            return false;
+                     }
+              }
+
+              return true;
+       }
+
+       static boolean isPrime(int n) {
+
+              if (prime(n) && prime(n - 2)) {
+                     return true;
+              } else {
+                     return false;
+              }
+       }
+
+       public static void main(String[] args) {
+
+              int n = 19;
+
+              if (isPrime(n)) {
+                     System.out.println("Yes");
+              } else if (isPrime(n)) {
+                     System.out.println("No");
+              }
+       }
+}

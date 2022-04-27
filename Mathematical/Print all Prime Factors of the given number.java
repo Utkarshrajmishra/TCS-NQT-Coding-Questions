072f52a0846bd0b46e95ca038bac04621c8ffbfa
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-27 19:28:59 +0530

    Create Print all Prime Factors of the given number.java

---
+++ Mathematical/Print all Prime Factors of the given number.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+
+  public static void primeFactor(int n) {
+    System.out.print("Prime Factors : ");
+    for (int i = 2; i * i <= n || n > 1; i++) {
+
+      if (n % i == 0) {
+
+        while (n % i == 0) {
+
+          System.out.print(i + " ");
+          n = n / i;
+        }
+      }
+
+    }
+  }
+  public static void main(String args[]) {
+    int n = 60;
+    primeFactor(n);
+  }
+}

e8e47d641a0916032e2f04a32ec23af3b09794c4
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-27 07:54:25 +0530

    Create Greatest of three numbers.java

---
+++ Mathematical/Greatest of three numbers.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String args[]) {
+    double num1 = 1.123, num2 = 1.124, num3 = 1.125;
+    System.out.println("The greatest of the three numbers is "+Math.max(num1, Math.max(num2, num3)));
+  }
+}

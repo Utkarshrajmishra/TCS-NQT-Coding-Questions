25573e939dd378f1e56b113be02810872f05145c
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-27 07:53:40 +0530

    Create Greatest of two numbers.java

---
+++ Mathematical/Greatest of two numbers.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String args[]) {
+    int num1 = 1;
+    int  num2 = 3;
+    System.out.println("The greatest of the two numbers is "+Math.max(num1, num2));
+  }
+}

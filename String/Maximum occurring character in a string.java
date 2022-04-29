fc523fb146a7efbddba51a09582a2189b5884118
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-29 08:38:23 +0530

    Create Maximum occurring character in a string.java

---
+++ String/Maximum occurring character in a string.java
@@ -0,0 +1,18 @@
+public class Main {
+   static char maxOccurringChar(String str) {
+      char ans = 'a';
+      int maxfreq = 0, n = str.length();
+      int count[] = new int[256];
+      for (int i = 0; i < n; i++) {
+         count[str.charAt(i)]++;
+         if (count[str.charAt(i)] > maxfreq) {
+            maxfreq = count[str.charAt(i)];
+            ans = str.charAt(i);
+         }
+      }
+      return ans;
+   }
+   public static void main(String[] args) {
+      String str = "takeuforward";
+      System.out.println("Maximum occurring character is " + maxOccurringChar(str));
+   }

1c37143b09752b599b51cfb49b1d939a96e3d91c
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-29 08:39:45 +0530

    Create Print all the duplicates in the string.java

---
+++ String/Print all the duplicates in the string.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        String str = "sinstriiintng";
+
+        int[] counts = new int[26];
+
+        for (int i = 0; i < str.length(); i++)
+            counts[str.charAt(i) - 'a']++;
+
+        for (int i = 0; i < 26; i++)
+            if (counts[i] > 1)
+                System.out.println((char)(i + 'a') + " - " + counts[i]);
+    }
+}

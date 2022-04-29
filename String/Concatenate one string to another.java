f09b6b40fbc781cfa7873e891408dcedc726e3cf
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-29 09:15:15 +0530

    Create Concatenate one string to another.java

---
+++ String/Concatenate one string to another.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+  
+class takeuforward {
+  
+    public static void main(String[] args)
+    {
+        String str1 = "Hello";
+        String str2 = "World";
+        
+        str1 = str1+ str2;
+        
+        System.out.print(str1);
+    }
+}
